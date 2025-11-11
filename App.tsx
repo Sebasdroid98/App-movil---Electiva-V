53b9798ba4583c1a70be25a35faa2c3e773d108d
Sebastian Zamora <zam98qui@gmail.com>
2025-11-10 21:09:12 -0500

    Se agrega componente Button con estilos basicos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './src/components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> andrees ...</Text>
-      <Text>aaaaaaaaaaaaaaaaaaaaaaa</Text>
+      <Text>Bienvenido a tu app de gestion de presupuesto</Text>
+      <Button label='Botón nuevo' backgroundColor='#ff0000'></Button>
+      <Button label='Botón nuevo x2' backgroundColor='#00ff00'></Button>
+      <Button label='Botón nuevo  x4' backgroundColor='#0000ff'></Button>
       <StatusBar style="auto" />
     </View>
   );
