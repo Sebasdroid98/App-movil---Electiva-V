451955eb516e7c24b488153c6e040b72349d8777
Sebastian Zamora <zam98qui@gmail.com>
2025-11-11 22:43:50 -0500

    Se integra vista FeaturesScreen en su version 1

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,15 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
+import FeaturesScreen from '../screens/FeaturesScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Feature" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Feature" component={FeaturesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
